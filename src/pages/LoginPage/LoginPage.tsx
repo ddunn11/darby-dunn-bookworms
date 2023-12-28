5cc38bb29ab7c67ac21fe380ae91843379b030f8
ddunn11 <148179140+ddunn11@users.noreply.github.com>
2023-12-28 14:12:58 -0800

    Added login form and login page content

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,41 @@
+import { Button } from "@mui/material";
 import "./LoginPage.scss";
 
 const LoginPage = () => {
   return (
-    <>
-      <div></div>
-    </>
+    <div className="login-page__container">
+      <div className="login-page__header-container">
+        <p>Don't have an account?</p>
+        <Button
+          variant="contained"
+          className="login-page__register-btn"
+          href="/create-account"
+        >
+          Register
+        </Button>
+      </div>
+      <div className="login-page__content">
+        <h1>Bookworms</h1>
+        <p>Login to access existing account</p>
+        <form className="login-page__form">
+          <div className="login-page__username-container">
+            <label htmlFor="username" className="login-page__input-label">
+              Username:
+            </label>
+            <input id="username" type="text" className="login-page__input" />
+          </div>
+          <div className="login-page__password-container">
+            <label htmlFor="password" className="login-page__input-label">
+              Password:
+            </label>
+            <input id="password" type="text" className="login-page__input" />
+          </div>
+          <Button variant="contained" className="login-page__login-btn">
+            Login
+          </Button>
+        </form>
+      </div>
+    </div>
   );
 };
 
